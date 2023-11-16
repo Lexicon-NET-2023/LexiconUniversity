fa205a306f5d94e0dbc511291783956375808987
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-11-16 13:48:06 +0100

    KalleAnka

---
+++ University.Web/Validations/KalleAnka.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace University.Web.Validations
+{
+    public class KalleAnka : ValidationAttribute
+    {
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            // var db = validationContext.GetRequiredService<UniversityContext>();
+
+            const string errorMessage = "Fail from Kalle Anka";
+
+            if (value is string input)
+            {
+                if (validationContext.ObjectInstance is StudentCreateViewModel model)
+                {
+                    return model.NameFirstName == "Kalle" && input == "Anka" ?
+                        ValidationResult.Success :
+                        new ValidationResult(errorMessage);
+                }
+
+            }
+            return new ValidationResult(errorMessage);
+
+        }
+    }
+}
